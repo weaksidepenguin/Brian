56835bdecc04ac824278bf99a064f14ed6ab25c4
weaksidepenguin <125506376+weaksidepenguin@users.noreply.github.com>
2023-04-14 20:49:25 -0400

    Update lab9.c

--- lab_assignment_9/lab9.c
+++ lab_assignment_9/lab9.c
@@ -3,21 +3,23 @@
 // RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+    int id;
+    char name;
+    int order;
+    struct RecordType* next;
 };
 
-// Fill out this structure
 struct HashType
 {
-
+    // create a pointer to a RecordType
+    struct RecordType* head;
 };
 
 // Compute the hash function
-int hash(int x)
+int hash(int x, int tableSize)
 {
-
+    // hash function is x % tableSize
+  return x % tableSize;
 }
 
 // parses input file to an integer array
@@ -69,26 +71,98 @@ void printRecords(struct RecordType pData[], int dataSz)
 	printf("\n\n");
 }
 
-// display records in the hash structure
-// skip the indices which are free
-// the output will be in the format:
-// index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+// insert record into the hash table
+void insertRecord(struct HashType *hashTable, struct RecordType *record, int tableSize)
 {
-	int i;
+    // call the hash function to get the index
+    // if the RecordType at that index is NULL
+        // set 'record' equal to the HashType pointer in the table at index
+    // else
+        // traverse to the end of the linkedlist and add 'record' to the end of it
+    // Call the hash function to get the index
+     // Call the hash function to get the index
+    // Call the hash function to get the index
+    int index = hash(record->id, tableSize);
 
-	for (i=0;i<hashSz;++i)
-	{
-		// if index is occupied with any records, print all
-	}
+    // If the RecordType at that index is NULL, set 'record' equal to the HashType pointer in the table at index
+    if (hashTable[index].head == NULL)
+    {
+        hashTable[index].head = record;
+    }
+    else
+    {
+        // Traverse to the end of the linkedlist and add 'record' to the end of it
+        struct RecordType *current = hashTable[index].head;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = record;
+    }
+
+}
+void displayRecordsInHash(struct HashType *hashTable, int tableSize)
+{
+    // for each entry in the table
+        // print the contents in that index
+        // The output should look like this (for example): "Index 1 -> 21715, Q, 16 -> 28876, a, 26 -> NULL"
+         printf("\nRecords in Hash Table:\n");
+    for (int i = 0; i < tableSize; i++)
+    {
+        if (hashTable[i].head != NULL)
+        {
+            printf("Index %d -> ", i);
+            struct RecordType *current = hashTable[i].head;
+            while (current != NULL)
+            {
+                printf("%d, %c, %d -> ", current->id, current->name, current->order);
+                current = current->next;
+            }
+            printf("NULL\n");
+        }
+    }
+    printf("\n");
 }
 
 int main(void)
 {
-	struct RecordType *pRecords;
-	int recordSz = 0;
+    struct RecordType *pRecords;
+    int recordSz = 0;
+    int hashTableSize = 10;
 
-	recordSz = parseData("input.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	// Your hash implementation
+    recordSz = parseData("input.txt", &pRecords);
+    printRecords(pRecords, recordSz);
+
+    // Initialize the hash table
+    // create a variable hashTableSize and assign it a value
+    // initialize a hashTable, use calloc (so everything is assigned to NULL)
+    // for each record in pRecords, insert it into the hash table using the insertRecord function
+    // call the display records function
+    // free all the allocated memory
+   // Initialize the hash table
+     // Initialize the hash table
+    struct HashType *hashTable = calloc(hashTableSize, sizeof(struct HashType));
+
+    for (int i = 0; i < recordSz; i++) {
+        insertRecord(hashTable, &pRecords[i], hashTableSize);
+    }
+
+    displayRecordsInHash(hashTable, hashTableSize);
+
+  
+    free(pRecords);
+
+   for (int i = 0; i < hashTableSize; i++) {
+        struct RecordType *current = hashTable[i].head;
+        while (current != NULL) {
+            struct RecordType *temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(hashTable);
+
+  
+
+    return 0;
 }
